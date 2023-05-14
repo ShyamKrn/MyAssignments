6606de9495271a56caa45316c928c9d2381e97fb
ShyamKrn <shyam.krishnan_cs19@gla.ac.in>
2023-05-14 11:34:36 +0530

    Connecting

---
+++ src/BasicDsAssignments/Assignment1Q1.java
@@ -0,0 +1,35 @@
+package BasicDsAssignments;
+
+import java.util.Scanner;
+class ArmstrongOrNot{
+    public boolean armstrongCheck(int num){
+        int temp = num;
+        String str = Integer.toString(temp);
+        int len = str.length();
+        temp = Integer.parseInt(str);
+        int rem = 0;
+        int sum = 0;
+        while(temp != 0){
+            rem = temp % 10;
+            temp = temp / 10;
+            sum += Math.pow(rem, len);
+        }
+
+        if(sum == num)
+            return true;
+        else
+            return false;
+    }
+}
+public class Assignment1Q1 {
+    public static void main(String[] args){
+        ArmstrongOrNot obj = new ArmstrongOrNot();
+        Scanner sc = new Scanner(System.in);
+        int armstrongNum = sc.nextInt();
+
+        if(obj.armstrongCheck(armstrongNum))
+            System.out.println(armstrongNum + " is an armstrong number");
+        else
+            System.out.println(armstrongNum + " is not an armstrong number");
+    }
+}
---
+++ src/BasicDsAssignments/Assignment1Q6.java
@@ -0,0 +1,36 @@
+package BasicDsAssignments;
+import java.util.Scanner;
+class Login{
+    String userId = "Shyam", password = "pass";
+    public String loginUser(String user, String pass){
+        Scanner sc = new Scanner(System.in);
+        if(user.equals(userId) && pass.equals(password)){
+            return "Welcome " + user;
+        }
+        else{
+            int numOfAttempts = 1;
+            while(numOfAttempts <= 2){
+                System.out.println("Enter userId");
+                String newUser = sc.next();
+                System.out.println("Enter password");
+                String newPass = sc.next();
+                if(newUser.equals(userId) && newPass.equals(password)){
+                    return "Welcome " + newUser;
+                }
+                numOfAttempts += 1;
+            }
+        }
+        return "Contact Admin";
+    }
+}
+public class Assignment1Q6 {
+    public static void main(String[] args) {
+        Login obj = new Login();
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter userId");
+        String user = sc.next();
+        System.out.println("Enter password");
+        String pass = sc.next();
+        System.out.println(obj.loginUser(user, pass));
+    }
+}
