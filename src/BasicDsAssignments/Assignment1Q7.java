400169d1d91109d58dd9c83dba6857eb9cf705c9
ShyamKrn <shyam.krishnan_cs19@gla.ac.in>
2023-05-14 12:14:22 +0530

    Solved Question 7

---
+++ src/BasicDsAssignments/Assignment1Q7.java
@@ -0,0 +1,25 @@
+package BasicDsAssignments;
+import java.util.Scanner;
+class SearchArray{
+    public boolean searchArray(int[] arr,int toCheckValue){
+        for(int i=0;i<arr.length;i++){
+            if(toCheckValue==arr[i]){
+                return true;
+            }
+        }
+        return false;
+    }
+}
+public class Assignment1Q7 {
+    public static void main(String[] args) {
+        int arr[] = { 5,12,14,6,78,19,1,23,26,35,37,7,52,86,47};
+        int valueToCheck = 19;
+        SearchArray obj=new SearchArray();
+        if(obj.searchArray(arr,valueToCheck)){
+            System.out.println("Value is present in the array");
+        }
+        else{
+            System.out.println("Value is not present in the array");
+        }
+    }
+}
